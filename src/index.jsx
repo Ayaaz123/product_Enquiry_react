b02504a5fbbb68f7f0e58fb01b2b29d0b379736d
Ayaaz123 <ayaazsalim123@gmail.com>
2024-10-15 20:12:44 +0530

    Switch to HashRouter

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,19 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import ProductInquiry from "./ProductInquiry";
-import './index.css';
-
-
-const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/inquiry", element: <ProductInquiry /> },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/inquiry" element={<ProductInquiry />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
 
